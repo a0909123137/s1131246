5b17c4ade0ef212a2bda1fe05b6ccdf9bf37f260
a0909123137 <a0909123137@gmail.com>
2025-12-01 15:56:26 +0800

    v1_林煥杰

--- app/src/main/java/tw/edu/pu/csim/tcyang/s1131246/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/s1131246/MainActivity.kt
@@ -12,11 +12,27 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import tw.edu.pu.csim.tcyang.s1131246.ui.theme.S1131246Theme
+import android.content.pm.ActivityInfo
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // 強制螢幕為直式(PORTRAIT)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+
         enableEdgeToEdge()
+
+        // 隱藏上方狀態列及下方巡覽列
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        val controller = WindowInsetsControllerCompat(window, window.decorView)
+        controller.hide(WindowInsetsCompat.Type.systemBars())
+        controller.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+
         setContent {
             S1131246Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
